9fab954d8d30df381ea37987048679a7ed61c83a
arup-sarkar-24 <1arupsarkar112@gmail.com>
2024-04-21 13:27:35 +0530

    Adding pagination and sorting

--- src/main/java/com/mycompany/employee_management/controller/EmployeeController.java
+++ src/main/java/com/mycompany/employee_management/controller/EmployeeController.java
@@ -3,12 +3,12 @@ package com.mycompany.employee_management.controller;
 import com.mycompany.employee_management.model.Employee;
 import com.mycompany.employee_management.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -18,20 +18,24 @@ public class EmployeeController {
 
     //display list of employee
     @GetMapping("/")
-    public String viewHomePage(Model model){
+    public String viewHomePage(Model model) {
+        return findPaginated(1,"firstName","asc",model);
+        /*
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+         */
     }
 
+    //add new Employee
     @GetMapping("/showNewEmployeeForm")
-    public String showNewEmployeeForm(Model model){
+    public String showNewEmployeeForm(Model model) {
         Employee employee = new Employee();
-        model.addAttribute("employee",employee);
+        model.addAttribute("employee", employee);
         return "new_employee";
     }
 
     @PostMapping("/saveEmployee")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee, Model model){
+    public String saveEmployee(@ModelAttribute("employee") Employee employee, Model model) {
         try {
             employeeService.saveEmployee(employee);
             return "redirect:/";
@@ -45,12 +49,40 @@ public class EmployeeController {
 
     //It is for update employee data
     @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model){
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
         Employee employee = employeeService.getEmployeeById(id);
         model.addAttribute("employee", employee);
         return "update_employee";
     }
 
+    //delete employee
+    @GetMapping("showFormForDelete/{id}")
+    public String showFormForDelete(@PathVariable(value = "id") long id) {
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
 
+    //add pagination add sorting String sortField, String sortDirection
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize,sortField,sortDir);
+        List<Employee> listEmployees = page.getContent();
+        //pagination
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
 
+        //shorting
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc")?"desc":"asc");
+
+        //total list
+        model.addAttribute("listEmployees", listEmployees);
+        return "index";
+    }
 }
--- src/main/java/com/mycompany/employee_management/services/EmployeeServiceImpl.java
+++ src/main/java/com/mycompany/employee_management/services/EmployeeServiceImpl.java
@@ -3,6 +3,10 @@ package com.mycompany.employee_management.services;
 import com.mycompany.employee_management.model.Employee;
 import com.mycompany.employee_management.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,5 +38,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
         return employee;
 
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending(): Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo -1, pageSize,sort);
+        return this.employeeRepository.findAll(pageable);
     }
 }
